51b4a822def1c62c4944af7f2aa6b2051b94b993
AlexGiuliano20 <alex.giuliano37@gmail.com>
2023-01-29 03:15:36 -0300

    limpieza de codigo

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,13 +1,7 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpClient,
-  HttpParams,
-  HttpHeaders,
-  HttpErrorResponse,
-} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
-import { throwError } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
